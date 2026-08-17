0be06bf1ea5d6ae3da1e44e9eba9e619f6464e7d
urakawa-seitaro <65066368+urakawa-seitaro@users.noreply.github.com>
2026-08-17 02:38:04 +0000

    chap18 finish

--- config/passport.js
+++ config/passport.js
@@ -8,10 +8,12 @@ const secret = "secretCuisine123";
 
 module.exports = function (app) {
   passport.serializeUser(function (user, done) {
+    //console.log("serializeUser");
     done(null, user.id);
   });
 
   passport.deserializeUser(async function (id, done) {
+    //console.log("deserializeUser");
     try {
       const user = await User.findById(id);
       done(null, user);
--- routes/logout.js
+++ routes/logout.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    //nullとするとことでセッションを削除＝logout
-  req.session = null;
-  res.redirect('/');
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
