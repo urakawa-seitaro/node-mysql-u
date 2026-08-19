c9c671c0ad878f2ff6bceb409b8f28e166e561ac
urakawa-seitaro <65066368+urakawa-seitaro@users.noreply.github.com>
2026-08-19 08:56:30 +0000

    complete chap19

--- routes/index.js
+++ routes/index.js
@@ -17,33 +17,44 @@ router.get('/', function (req, res, next) {
   //const userId = req.session.userid;
   //const isAuth = Boolean(userId);
   const isAuth = req.isAuthenticated();
-  knex("tasks")
-    .select("*")
-    .then(function (results) {
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results,
-        isAuth: isAuth,
+  //const userId = req.user.id;
+  if (isAuth){
+    const userId = req.user.id;
+    knex("tasks")
+      .select("*")
+      .where({user_id: userId})
+      .then(function (results) {
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+          isAuth: isAuth,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('index', {
+          title: 'ToDo App',
+          isAUth: isAuth,
+          errorMessage: [err.sqlMessage],
+        });
       });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render('index', {
-        title: 'ToDo App',
-        isAUth: isAuth,
-        errorMessage: [err.sqlMessage],
-      });
+  } else {
+    res.render('index',{
+      title: 'ToDo App',
+      isAuth: isAuth,
     });
-});
+  }    
+  });
 
 //chapter 14
 router.post('/', function (req, res, next) {
   //const userId = req.session.userid;
   //const isAuth = Boolean(userId);
   const isAuth = req.isAuthenticated();
+  const userId = req.user.id;
   const todo = req.body.add;
   knex("tasks")
-    .insert({user_id: 1, content: todo})
+    .insert({user_id: userId, content: todo})
     .then(function () {
       res.redirect('/')
     })
